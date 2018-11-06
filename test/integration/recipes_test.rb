f03411f526153dc8fff442b27d70802180484693
Craig Eastwood <craigdavideastwood@gmail.com>
2018-11-06 12:41:32 +0000

    Added TDD for show recipe and gravatar image

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
   def setup
     @aChef = Chef.create!(chefname: "Craig", email: "cde@mail.com.au")
-    @aRecipe = Recipe.create!(name: "best bbq bangers", description: "awesome bbq sausages.", chef: @aChef)
+    @aRecipe = Recipe.create!(name: "Best bbq bangers", description: "awesome bbq sausages.", chef: @aChef)
     @aRecipe2 = @aChef.recipes.build(name: "chocalate cake", description: "fast and easy chocalate cake")
     @aRecipe2.save
   end
@@ -16,7 +16,17 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get the recipes list" do
     get recipes_path
     assert_template 'recipes/index'
+    # search for links to the specific recipe pages (a tags) - which are on the Recipe names
+    assert_select "a[href=?]", recipe_path(@aRecipe), text: @aRecipe.name
+    assert_select "a[href=?]", recipe_path(@aRecipe2), text: @aRecipe2.name
+  end
+  
+  test "should get the recipes show" do
+    get recipe_path(@aRecipe)
+    assert_template 'recipes/show'
+    # search for the recipe name, description and chef name in the body of the page
     assert_match @aRecipe.name, response.body
-    assert_match @aRecipe2.name, response.body
+    assert_match @aRecipe.description, response.body
+    assert_match @aChef.chefname, response.body
   end
 end
