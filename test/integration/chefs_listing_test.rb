d78c31b43b2c754daf4f7cae8e1ed20e47067875
Craig Eastwood <craigdavideastwood@gmail.com>
2019-01-06 10:04:12 +0000

    Added tests for chefs listing

---
+++ test/integration/chefs_listing_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ChefsListingTest < ActionDispatch::IntegrationTest
+  def setup
+    @aChef = Chef.create!(chefname: "Craig", email: "cde@mail.com.au",
+                    password: "password", password_confirmation: "password")
+    @aChef2 = Chef.create!(chefname: "John", email: "john@mail.com.au",
+                    password: "password", password_confirmation: "password")
+    @aRecipe = Recipe.create!(name: "Best Bbq Bangers", description: "awesome bbq sausages.", chef: @aChef)
+  end
+  
+  test "should get the chefs list page" do
+    get chefs_path
+    assert_response :success
+  end
+  
+  test "should get the chefs listing" do
+    get chefs_path
+    assert_template 'chefs/index'
+    # search for links to the specific chef pages (a tags) - which are on the Chefs
+    assert_select "a[href=?]", chef_path(@aChef), text: @aChef.chefname
+    assert_select "a[href=?]", chef_path(@aChef2), text: @aChef2.chefname
+    assert_match "1 recipe", response.body
+    assert_match "0 recipes", response.body
+  end
+end
