cd6d57d667a248a0d8a1919e21851135b4e44d5e
Craig Eastwood <craigdavideastwood@gmail.com>
2019-02-03 03:31:59 +0000

    Prepare app for heroku production with actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://craig-myrecipes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://craig-myrecipes.herokuapp.com', 'http://craig-myrecipes.herokuapp.com']
 end
